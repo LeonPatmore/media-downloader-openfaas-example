1c0768325b7e09c8be5d63122f3612a3bba9d108
Leon Patmore <leon.patmore@bath.edu>
2022-08-10 20:49:43 +0100

    using http.

--- examples/python/handler.py
+++ examples/python/handler.py
@@ -7,12 +7,17 @@ s3_client = boto3.client('s3')
 logging.basicConfig(level=logging.DEBUG)
 
 
-def handle(req):
+def handle(event, context):
     # s3_client.upload_file(file_name, bucket, object_name)
 
-    logging.info("Hello! You said: " + req)
-    os.environ["content_type"] = "application/json"
+    logging.info(f"Hello! You said: " + event + " | " + context)
 
     return {
-        "id": "abc123"
+        "statusCode": 200,
+        "body": {
+            "key": "value"
+        },
+        "headers": {
+            "Content-Type": "application/json"
+        }
     }
