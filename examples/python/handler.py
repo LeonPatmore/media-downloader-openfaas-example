95f59fd17791a03dde95a799d2ca271cb1fa2317
Leon Patmore <leon.patmore@bath.edu>
2022-08-16 20:50:28 +0100

    testing s3 client.

--- examples/python/handler.py
+++ examples/python/handler.py
@@ -9,11 +9,18 @@ access_key_id = get_secret("accessKeyId")
 secretAccessKey = get_secret("secretAccessKey")
 logging.info(f"AWS access key is {access_key_id}")
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client('s3',
+                         region_name="eu-west-1",
+                         aws_access_key_id=access_key_id,
+                         aws_secret_access_key=secretAccessKey)
 
 
 def handle(event, context):
+
     # s3_client.upload_file(file_name, bucket, object_name)
+
+    with open('/var/openfaas/secrets/accessKeyId', 'rb') as data:
+        s3_client.upload_fileobj(data, 'mybucket', 'example')
 
     logging.info(f"Hello! You said: " + str(event) + " | " + str(context))
 
