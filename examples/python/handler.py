9452176750f0713e92470760993c89d385093375
Leon Patmore <leon.patmore@bath.edu>
2022-08-16 21:06:48 +0100

    debug

--- examples/python/handler.py
+++ examples/python/handler.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -13,11 +14,14 @@ logging.info(f"AWS access key is [ {access_key_id} ]")
 
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 assert BUCKET_NAME
-
 logging.info(f"Bucket name is [ {BUCKET_NAME} ]")
 
+AWS_REGION = os.environ.get("AWS_REGION", "eu-west-1")
+assert AWS_REGION
+logging.info(f"AWS region [ {AWS_REGION} ]")
+
 s3_client = boto3.client('s3',
-                         region_name="eu-west-1",
+                         region_name=AWS_REGION,
                          aws_access_key_id=access_key_id,
                          aws_secret_access_key=secretAccessKey)
 
@@ -26,6 +30,11 @@ def handle(event, context):
     with open('/var/openfaas/secrets/accessKeyId', 'rb') as data:
         s3_client.upload_fileobj(data, BUCKET_NAME, 'example')
 
+    logging.info(json.dumps(event.body))
+    logging.info(json.dumps(event.headers))
+    logging.info(json.dumps(event.method))
+    logging.info(json.dumps(event.query))
+    logging.info(json.dumps(event.path))
     logging.info(f"Hello! You said: " + str(event) + " | " + str(context))
 
     return {
